0e6a1c013fdb2065405357f7d294e9e660a3fd24
Kirito <brandnfuentes06@gmail.com>
2020-09-15 12:42:09 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,39 @@
+
+var name = "Kirito"
+let lastname = "Odinson"
+
+console.log(name + lastname);
+
+// falta agregar que es un dato aleatorio el false user
+switch(name) {
+    case 'Kirito':
+        console.log('true user')
+        break;
+    case '!=':
+        console.log('false user')
+        break;
+}
+
+document.write(name, lastname)
+
+var Word = "Rut"
+let Number = "20.514.004-2"
+
+console.log(Word, Number)
+
+// falta agregar que es un dato aleatorio el false user
+switch(Number) {
+    case '20.514.004-2':
+        console.log('true user')
+        break;
+    case '20.514.004-':
+        console.log('false user')
+        break;
+}
+
+document.write(Word, Number)
+
+
+
+
+
